8112aa47aaf91f334958699990e836141a0f81f6
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2022-11-21 13:30:59 +0700

    Create list view for show list coffeeshop

--- Coffeeshop/View/CoffeeshopListView.swift
+++ Coffeeshop/View/CoffeeshopListView.swift
@@ -19,8 +19,27 @@ struct CoffeeshopListView: View {
     var body: some View {
         NavigationStack {
             List(coffeeSearchResults) { result in
-                /*@START_MENU_TOKEN@*/Text(result.image)/*@END_MENU_TOKEN@*/
-            }
+                HStack() {
+                    
+                    Image(result.image)
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(15)
+                    
+                    VStack(alignment: .leading) {
+                        //TODO: Styling Text
+                        
+                        Text(result.name)
+                            .font(.system(size: 15, design: .rounded))
+                            .fontWeight(.black)
+                        Text(result.location)
+                            .multilineTextAlignment(.leading)
+                        Text("Rating: 4/5")
+                    }
+                } //: HStack
+//                /*@START_MENU_TOKEN@*/Text(result.image)/*@END_MENU_TOKEN@*/
+            } //: List CoffeeSearch
+            .navigationTitle("Coffeeshop")
         } //: NAVIGATION
     } //: View
 }
