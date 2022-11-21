cb6652b6b59427e9210f120cd9d1b9ce209c6967
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2022-11-21 11:08:11 +0700

    Init Cofeeshop project

---
+++ Coffeeshop/CoffeeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeshopApp.swift
+//  Coffeeshop
+//
+//  Created by ihsan.satriawan on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
